fa1bb99488cab02c310df2999c79883b187ff5e8
11111Asif <asif040502@gmail.com>
2025-09-10 22:34:50 -0700

    added to number even

--- number.js
+++ number.js
@@ -1,6 +1,7 @@
 const numbers = [12, 98, 5, 41, 23, 78, 46];
 let even = []
-for(let i = 0; i< numbers.length; i ++){
+
+for(let i = 0; i < numbers.length; i++){
     if(numbers[i] % 2 === 0){
         even.push(numbers[i])
     }
